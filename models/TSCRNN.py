53e118c606ed27ffd071b6e9013b300702933881
Jay <45455827+JieJayCao@users.noreply.github.com>
2022-12-08 15:33:41 +0800

    Create TSCRNN.py

---
+++ models/TSCRNN.py
@@ -0,0 +1,56 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import numpy as np
+import copy
+
+
+class Config(object):
+
+ 
+    def __init__(self, dataset):
+        self.model_name = 'TSCRNN'
+        self.train_path = dataset + '/data/train.txt'                                
+        self.dev_path = dataset + '/data/dev.txt'                                    
+        self.test_path = dataset + '/data/test.txt'                                  
+        self.class_list = [x.strip() for x in open(
+            dataset + '/data/class.txt', encoding='utf-8').readlines()]                        
+        self.save_path = dataset + '/saved_dict/' + self.model_name + '.ckpt'        
+        self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')   
+        self.learning_rate = 0.001                                       
+        self.num_classes = len(self.class_list)                  
+        self.num_epochs = 30                                            
+        self.batch_size = 128                                           
+   
+
+class Model(nn.Module):
+    def __init__(self, config): 
+    
+        super(Model, self).__init__()
+        self.conv1 = nn.Conv1d(in_channels= 15, out_channels= 64 , kernel_size=3, stride=1, padding=1)
+        self.bn1 = nn.BatchNorm1d(64,affine = True)
+        self.conv2 = nn.Conv1d(in_channels= 64, out_channels= 64 , kernel_size=3, stride=1,  padding=1)
+        self.bn2 = nn.BatchNorm1d(64,affine = True)
+        self.lstm = nn.LSTM(375, 256, 2, bidirectional=True, batch_first=True, dropout=0.3)
+        self.dropout = nn.Dropout(0.3)
+        self.out = nn.Linear(in_features=512, out_features=config.num_classes) 
+      
+        
+    def forward(self, t):        
+       
+        t = self.conv1(t)
+        t = self.bn1(t)
+        t = F.relu(t)
+        t = F.max_pool1d(t, kernel_size=2, stride=2)
+        
+        t = self.conv2(t)
+        t = self.bn2(t)
+        t = F.relu(t)
+        t = F.max_pool1d(t, kernel_size=2, stride=2)
+
+        t,_ = self.lstm(t)
+        t = self.dropout(t)
+        
+        t = self.out(t[:, -1, :]) 
+        return t
+
